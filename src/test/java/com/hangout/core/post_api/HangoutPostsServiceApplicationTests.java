a9bdf239d9bc4c6eaaae8f7df8108e3581bfe9ee
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-01-22 20:45:32 +0530

    tests are working but more work is needed, need to mock real db connection with test containers

--- src/test/java/com/hangout/core/post_api/HangoutPostsServiceApplicationTests.java
+++ src/test/java/com/hangout/core/post_api/HangoutPostsServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.hangout.core.post_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class HangoutPostsServiceApplicationTests {
     @Test
     void contextLoads() {
